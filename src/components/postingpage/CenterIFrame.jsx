4ff9fa8f0bdf7709e38c991c51f22ebd60cbfdfd
henry-mcdonald <a.henrymcdonald.a@gmail.com>
2021-03-22 19:25:36 -0500

    add some stubbed components to display page

---
+++ src/components/postingpage/CenterIFrame.jsx
@@ -0,0 +1,11 @@
+const CenteriFrame = () => {
+
+
+    return (
+        <div style={{ backgroundColor: "white", height: "100vh" }}>
+            <h1>Hello from CenteriFrame</h1>
+        </div>
+    )
+}
+
+export default CenteriFrame

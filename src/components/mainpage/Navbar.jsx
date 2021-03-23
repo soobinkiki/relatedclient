bd11f98184fff980a10b25a6a6f95749716c6b65
soobinkiki <j.sb.parkk@gmail.com>
2021-03-23 09:20:11 -0700

    Auth done

--- src/components/mainpage/Navbar.jsx
+++ src/components/mainpage/Navbar.jsx
@@ -1,6 +1,7 @@
 // import { Button } from 'react-bootstrap'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom'
+import Logo from '../../images/relatedlogo.png'
 import '../../main.css'
 
 
@@ -8,9 +9,10 @@ const Navbar = (props) => {
     
     const loggedIn = (
         <div className="navlogo-container">
-            <Link to='/' id="mainLink"><h1 id="mainNavText">RelateD</h1></Link>
+            <Link to='/' id="mainNavLink"><img id="mainNavText" src={Logo} alt="related-logo" /></Link>
+            {/* <Link to='/' id="mainNavLink"><h1 id="mainNavText">RelateD</h1></Link> */}
             <nav className="mainNavList">
-                <Link to='/profile' id="mainLink"><h1 id="NavText">Proflile</h1></Link>
+                <Link to='/profile' id="mainLink"><h1 id="NavText">Profile</h1></Link>
                 <Link to='/newsfeed' id="mainLink"><h1 id="NavText">Newsfeed</h1></Link>
                 <Link to='/' id="mainLink" onClick={props.handleLogout}><h1 id="NavText">Logout</h1></Link>
                 <Link to='/contact' id="mainLink"><h1 id="NavText" className="NavTextContact">Contact</h1></Link>
@@ -20,13 +22,12 @@ const Navbar = (props) => {
 
     const loggedOut = (
         <div className="navlogo-container">
-            <Link to='/' id="mainLink"><h1 id="mainNavText">RelateD</h1></Link>
+            <Link to='/' id="mainNavLink"><img id="mainNavText" src={Logo} alt="related-logo" /></Link>
             <Link to='/contact' id="mainLink"><h1 id="NavText" className="NavTextContact">Contact</h1></Link>
         </div>
     )
 
     return (
-        
         <div>
             {props.currentUser ? loggedIn : loggedOut}
         </div>

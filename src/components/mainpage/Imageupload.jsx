575a9703b050facb61744852147cdfbf78a39892
dlashinsky <da.lashinsky@gmail.com>
2021-03-25 17:29:34 -0700

    pushing changes

--- src/components/mainpage/Imageupload.jsx
+++ src/components/mainpage/Imageupload.jsx
@@ -12,16 +12,21 @@ const ImageUpload = () => {
             setFileData(e.target.files[0])
             setImages(e.target.value)
         }
-    
+        console.log(fileData)
+
         const handleSubmit = async (e) => {
             try {
                 e.preventDefault()
-        
+
+                const token = localStorage.getItem('jwtToken')
+                const authHeaders =  {
+                    'Authorization': token
+                }
+
                 const formdata = new FormData()
-        
                 formdata.append("image", fileData)
-        
-                const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/image`, formdata)
+                
+                const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/image-upload`, formdata, { headers: authHeaders })
                 console.log(response);
     
             } catch(error) {
@@ -31,15 +36,24 @@ const ImageUpload = () => {
     
     return (
         <div>
-            <input type="file"
-                         value={images}
-                         name="file"
-                         accept="images/*"
-                         onChange={handleFileChange}
-                         placeholder="upload image"
-                         isRequired={true}
+            <form onSubmit={handleSubmit}>
+                <input       
+                type="file"
+                value={images}
+                name="file"
+                accept="images/*"
+                onChange={handleFileChange}
+                placeholder="upload image"
+                isRequired={true}
+                />
+                <input
+                type="submit"
+                value="Upload"
+                />
 
-            />
+            </form>
+            
+            
         </div>
     )
 }
